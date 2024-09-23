63e1c8dc0143ae8c2d5563be59f2c9f07f26d128
Alexandros Alexandrov <59898265+AlexandrosAlexandrov@users.noreply.github.com>
2024-09-23 21:27:34 +0300

    Project Init

---
+++ PokemonExplorer/PokemonExplorer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokemonExplorer
+//
+//  Created by Alexandros Alexandrov on 23/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokemonExplorer/PokemonExplorer/PokemonExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonExplorerApp.swift
+//  PokemonExplorer
+//
+//  Created by Alexandros Alexandrov on 23/9/24.
+//
+
+import SwiftUI
+
+@main
+struct PokemonExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
